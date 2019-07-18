27884731098927c158586c06e88d25243703df71
Domenica Cañizares <Domenica Caizares@Domenica>
2019-07-18 16:47:43 -0500

    Creacion proyecto

---
+++ src/ec/edu/ups/principal/Matriz.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.principal;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Domenica Cañizares
+ */
+public class Matriz {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
+        
+        System.out.println("Ingrese el numero de filas \n");
+        int fila = sn.nextInt();
+        
+        
+    }
+    
+}
---
+++ src/ec/edu/ups/principal/SumaMatriz.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.principal;
+
+/**
+ *
+ * @author Domenica Cañizares
+ */
+public class SumaMatriz extends Thread {
+
+    private final int[][] matrizA;
+    private final int[][] matrizB;
+    private final int[][] matrizRes;
+    private final int minFilas;
+    private final int maxFilas;
+
+    public SumaMatriz(int[][] matrizA, int[][] matrizB, int[][] matrizRes, int minFilas, int maxFilas) {
+        this.matrizA = matrizA;
+        this.matrizB = matrizB;
+        this.matrizRes = matrizRes;
+        this.minFilas = minFilas;
+        this.maxFilas = maxFilas;
+    }
+
+    public void run() {
+
+        for (int i = minFilas; i < maxFilas; i++) {
+
+            for (int j = 0; j < matrizRes[i].length; j++) {
+                matrizRes[i][j] = matrizA[i][j] + matrizB[i][j];
+            }
+
+        }
+
+    }
+
+}
